113be2ea56480e63cb94d00e9cf8b0e218b52042
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-08-28 21:32:39 +0300

    Update 23/08/28
    Update CommentsList.jsx

--- components/CommentsList/CommentsList.jsx
+++ components/CommentsList/CommentsList.jsx
@@ -3,7 +3,7 @@ import { ScrollView, StyleSheet } from "react-native";
 import { CommentsListItem } from "../CommentsListItem/CommentsListItem";
 
 export const CommentsList = ({comments}) => <ScrollView style={styles.container}>
-    {comments.map((comment, index) => <CommentsListItem
+    {comments.map(comment => <CommentsListItem
         key={comment.id}
         comment={comment}
     />)}    
